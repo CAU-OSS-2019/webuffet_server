4c1d0ebbdd470f762a67f0920865be31c8856af3
ihooni <1000jaman@naver.com>
2019-05-17 03:29:15 +0900

    Apply new provider sturcture to app main.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import AppServiceProvider from 'Provider/AppServiceProvider';
 import { server_config } from 'Configs/server';
 
-const app = AppServiceProvider.boot();
+// Boot our app
+const app_service = new AppServiceProvider();
+app_service.boot();
 
+const app = app_service.getService();
 app.listen(server_config.port);
