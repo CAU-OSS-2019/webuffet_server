d8918aa4a8c200ade5f2d42e70c89374694e37a5
ihooni <1000jaman@naver.com>
2019-05-22 02:18:44 +0900

    Change theme schema

--- src/database/schemas/theme.js
+++ src/database/schemas/theme.js
@@ -6,14 +6,12 @@
  */
 
 import mongoose from 'mongoose';
-
+ 
 const themeSchema = new mongoose.Schema({
-  hash_id: { type: String, required: true },
-  url: { type: String, required: true },            // url of customized website
-  style_data: { type: String, required: true },     // customized style data
-  title: { type: String, required: true },
-  thumbnail: { type: String },                      // path of thumbnail image file
-  edited_date: { type: Date, default: Date.now }    // last edited date
+  url: { type: String, required: true },    // url of customized website
+  style_data: { type: mongoose.Schema.Types.Mixed },    // customized style data
+  title: { type: String },
+  thumbnail: { type: String },  // path of thumbnail image file
 });
 
 export default themeSchema;
