83eae69021b6dcf0e6a5cb808cbca4b92bb48be9
ihooni <1000jaman@naver.com>
2019-05-21 17:42:22 +0900

    Schema update

--- src/database/schemas/theme.js
+++ src/database/schemas/theme.js
@@ -8,10 +8,12 @@
 import mongoose from 'mongoose';
 
 const themeSchema = new mongoose.Schema({
+  hash_id: { type: String, required: true },
   url: { type: String, required: true },            // url of customized website
   style_data: { type: String, required: true },     // customized style data
-  title: { type: String },
-  thumbnail: { type: String }                       // path of thumbnail image file
+  title: { type: String, required: true },
+  thumbnail: { type: String },                      // path of thumbnail image file
+  edited_date: { type: Date, default: Date.now }    // last edited date
 });
 
 export default themeSchema;
--- src/database/schemas/user.js
+++ src/database/schemas/user.js
@@ -11,7 +11,6 @@ import themeSchema from './theme';
 const userSchema = new mongoose.Schema({
   account_id: { type: String, required: true, unique: true },   // hash id of google account
   email: { type: String, required: true, lowercase: true },     // google email
-  name: { type: String, required: true },                       // google user name
   themes: [ { type: themeSchema, required: true } ]             // all themes which are created by this user
 });
 
