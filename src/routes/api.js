a051111d8e12339812acc6b6e15a796b844ffd9a
ihooni <1000jaman@naver.com>
2019-05-25 03:59:37 +0900

    Add middleware for update specific theme
    # Add new api route for updating theme

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import ThemeListMiddleware from 'Middleware/ThemeListMiddleware';
 import ThemeCreateMiddleware from 'Middleware/ThemeCreateMiddleware';
+import ThemeUpdateMiddleware from 'Middleware/ThemeUpdateMiddleware';
 
 const router = express.Router();
 
@@ -34,6 +35,24 @@ router.post('/storage/user', async (req, res) => {
       const created_theme_id = await middleware.create(req_json.user, req_json.theme_url);
 
       res.json({ err: false, theme_id: created_theme_id });
+    } catch (err) {
+      res.json({ err: true, msg: err });
+    }
+  }
+});
+
+// update specific theme
+router.put('/storage/user/theme', async (req, res) => {
+  const req_json = req.body;
+  const middleware = new ThemeUpdateMiddleware();
+
+  if (!req_json.hasOwnProperty('user') || !req_json.hasOwnProperty('theme')) {
+    res.json({ err: true, msg: "invalid parameter" });
+  } else {
+    try {
+      await middleware.update(req_json.user, req_json.theme);
+
+      res.json({ err: false });
     } catch (err) {
       res.json({ err: true, msg: err });
     }
